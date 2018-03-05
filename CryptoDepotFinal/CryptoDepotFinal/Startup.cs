afad1db66b36f819ddc75be11ef3b0adaacb7c6c
Unknown <mjhatley21@gmail.com>
2018-03-05 11:22:14 -0500

    start of final project
    with identity, entity, and MVC

---
+++ CryptoDepotFinal/CryptoDepotFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CryptoDepotFinal.Startup))]
+namespace CryptoDepotFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
