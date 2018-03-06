4a9e67bf2153ea554573d84164dddd55c94592c8
Unknown <mjhatley21@gmail.com>
2018-03-06 16:31:49 -0500

    two new databases for the simulation and linked them with a foreign key
    The foreign key links the IdSimulation

--- CryptoDepotFinal/CryptoDepotFinal/Controllers/HomeController.cs
+++ CryptoDepotFinal/CryptoDepotFinal/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +28,34 @@ namespace CryptoDepotFinal.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GetData()
+        {
+            ViewBag.Message = "Your contact page.";
+            HttpWebRequest request = WebRequest.CreateHttp("https://coinbin.org/coins");
+            string url_history = "https://coinbin.org/lbc/history";
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string ApiText = rd.ReadToEnd();
+            string data = rd.ToString();
+            JObject o = JObject.Parse(ApiText);
+            
+            
+            ViewBag.ApiText = "The Bitcoin is" + o["coins"];
+
+
+
+
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            ViewBag.Message = "Your Data page.";
+            return GetData();
         }
     }
 }
