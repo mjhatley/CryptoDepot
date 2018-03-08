8251115826e01e99f5fd9c364315396c5b1d6f4a
Unknown <mjhatley21@gmail.com>
2018-03-08 11:17:30 -0500

    JSON data is parsed and listed for the wanted parameters
    .

--- CryptoDepotFinal/CryptoDepotFinal/Controllers/HomeController.cs
+++ CryptoDepotFinal/CryptoDepotFinal/Controllers/HomeController.cs
@@ -7,16 +7,37 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using CryptoDepotFinal.Models;
-
+using System.Linq;
 namespace CryptoDepotFinal.Controllers
 {
     public class HomeController : Controller
     {
+        List<CoinDetail> lcn = new List<CoinDetail>();
         public ActionResult Index()
         {
+            lcn = GetCoinData();
+          return RedirectToAction("GetCoins") ;
+         //   return View();
+        }
+
+
+
+
+        public ActionResult Invest(double? amount)
+        {
+
+            List<CoinDetail> TempData = GetCoinData();
+
+            List<CoinDetail> tp =( from elt in TempData.Where(x => double.Parse(x.usd) <= amount) select elt).ToList();
             
-          
-            return View();
+            //Where(x => double.Parse(x.usd) >= amount);
+            if (amount != null)
+            {
+                return View(tp);
+            }
+                
+            else
+                return View(TempData);
         }
 
         public ActionResult About()
@@ -41,12 +62,28 @@ namespace CryptoDepotFinal.Controllers
             request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             StreamReader rd = new StreamReader(response.GetResponseStream());
-
             string data = rd.ReadToEnd();
             JObject o = JObject.Parse(data);
+            List<JToken> coinsRaw = o["coins"].ToList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                JToken j = coinsRaw[i];
+
+                List<JToken> props = j.First.ToList();
+                ViewBag.names += props[1];
+
+            }
+
+
+
+
+
+            //string data = rd.ReadToEnd();
+            //JObject o = JObject.Parse(data);
             
             
-            ViewBag.ApiText = "The Bitcoin is" + o["coins"]["$$$"];
+            //ViewBag.ApiText = "The Bitcoin is" + o["coins"]["$$$"];
 
 
             
@@ -59,5 +96,71 @@ namespace CryptoDepotFinal.Controllers
             ViewBag.Message = "Your Data page.";
             return GetData();
         }
+
+        public List<CoinDetail> GetCoinData()
+        {
+            string url = "https://coinbin.org/coins";
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            JObject o = JObject.Parse(data);
+
+            List<JToken> coins = o["coins"].ToList();
+            lcn = new List<CoinDetail>();
+            CoinDetail cn;
+            foreach (var elt in o["coins"])
+            {
+                JToken a = elt.First();
+                cn = new CoinDetail();
+                cn.name = a["name"].ToString();
+                cn.btc = a["btc"].ToString();
+                cn.rank = a["rank"].ToString();
+                cn.ticker = a["ticker"].ToString();
+                cn.usd = a["usd"].ToString();
+
+                lcn.Add(cn);
+
+            }
+
+            //  ViewBag.coins = items;
+            return lcn;
+        }
+
+            public ActionResult GetCoins()
+        {
+            string url = "https://coinbin.org/coins";
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            JObject o = JObject.Parse(data);
+
+            List<JToken> coins = o["coins"].ToList();
+            lcn = new List<CoinDetail>();
+            CoinDetail cn;
+            foreach (var elt in o["coins"])
+            {
+                JToken a = elt.First();
+                cn = new CoinDetail();
+                cn.name = a["name"].ToString();
+                cn.btc = a["btc"].ToString();
+                cn.rank = a["rank"].ToString();
+                cn.ticker = a["ticker"].ToString();
+                cn.usd =  a["usd"].ToString();
+
+                lcn.Add(cn);
+
+            }
+
+            //  ViewBag.coins = items;
+            return View(lcn);
+
+        }
+    
     }
 }
